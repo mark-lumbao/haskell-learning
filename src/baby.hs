dc7a944a3f461161e351f85fe872bf9aff91ba99
Mark Lumbao <markanthonylumbao@gmail.com>
2021-08-20 10:44:37 +0800

    feat: Added type declarations

--- src/baby.hs
+++ src/baby.hs
@@ -1,16 +1,25 @@
+doubleMe :: (Num x) => x -> x
 doubleMe x = x + x
 
+doubleUs :: (Num x) => x -> x -> x
 doubleUs x y = doubleMe x + doubleMe y
 
+doubleSmallNumber :: (Ord x, Num x) => x -> x
 doubleSmallNumber x =
   if x > 100
     then x
     else x*2
 
+getEven :: (Integral x) => [x] -> [x]
 getEven list = [x | x <- list, even x]
 
+multiplicationTable :: (Enum x, Num x) => x -> Int -> [x]
 multiplicationTable factor limit = take limit [factor, factor*2..]
 
+type Triangles = [(Integer, Integer, Integer)]
+
+triangles :: Triangles
 triangles = [ (a,b,c) | c <- [1..10], b <- [1..10], a <- [1..10] ]
 
+rightTriangles :: Triangles
 rightTriangles = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2]
