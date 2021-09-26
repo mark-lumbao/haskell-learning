15fada546b1b09610bc3acc33c60eeb0dec8c129
Mark Anthony Lumbao <markanthonylumbao@gmail.com>
2021-09-26 08:57:26 +0800

    chore: Learned Filter, Map, and a few lambdas

--- src/higher-order-functions.hs
+++ src/higher-order-functions.hs
@@ -11,3 +11,19 @@ compareWithHundred' = (`compare` 100) -- Infix
 -- Functions can take functions as parameters and also return functions.
 applyTwice :: (a -> a) -> a -> a
 applyTwice f x = f (f x)
+
+-- Mapping
+sampleMap = map (+3) [1..3]
+
+-- Filterring
+sampleFilter = filter odd [1..100]
+
+chain :: (Integral a) => a -> [a]
+chain 1 = [1]
+chain n
+    | even n =  n:chain (n `div` 2)
+    | odd n  =  n:chain (n*3 + 1)
+
+-- Lambdas
+numLongChains :: Int
+numLongChains = length (filter (\xs -> length xs > 15) (map chain [1..100]))
